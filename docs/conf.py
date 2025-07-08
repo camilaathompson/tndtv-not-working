eaf4f66cd2ee227d8706fe0f8f774e467ca7746a
camilaathompson <camilaathompson@outlook.com>
2025-07-08 12:36:16 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,4 +7,8 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
-html_static_path = ['_static'
+html_static_path = ['_static']
+
+html_js_files = [
+    'livechat.js',
+]
